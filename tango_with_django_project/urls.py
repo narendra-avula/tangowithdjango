a2a7c86bdff7ba35de91b9c2cbeaa8499b90571d
narendraavula <narendraavula2@gmail.com>
2016-05-18 17:03:50 +0530

    First Commit

---
+++ tango_with_django_project/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += patterns(
+#         'django.views.static',
+#         (r'^media/(?P<path>.*)',
+#         'serve',
+#         {'document_root': settings.MEDIA_ROOT}), )
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
